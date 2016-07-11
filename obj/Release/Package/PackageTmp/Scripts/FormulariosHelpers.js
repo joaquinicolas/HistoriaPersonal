00d8de6d12e36656180294800e911ca9db053d00
Joaquin Noé <joako.n96@gmail.com>
2016-07-11 09:31:16 -0300

    ultimo

---
+++ obj/Release/Package/PackageTmp/Scripts/FormulariosHelpers.js
@@ -0,0 +1,86 @@
+function parsearFecha(fecha) {
+    var fecha2 = "";
+    if (fecha.length == 10) {
+        fecha2 = fecha.substring(6, 10) + "-" + fecha.substring(3, 5) + "-" + fecha.substring(0, 2);
+    }
+    return fecha2;
+}
+
+function setearCheckbox(chk) {
+    $("input[name='" + chk + "']").val($('#' + chk).prop("checked"));
+}
+
+function setearCheckboxes() {
+    $.each(
+        $("#form").find("input[type='checkbox']"),
+        function (i, e) {
+            var seleccionado = $(e).prop("checked");
+            console.log($(e).attr("id") + " " + seleccionado);
+            $("input[name='" + $(e).attr("id") + "']").val(seleccionado);
+        }
+    );
+}
+
+var posteandoFormulario = false;
+function postForm(action) {
+    $.ajax({
+        method: "POST",
+        url: "/HP/Pasos/" + action + "/",
+        data: $("#form").serialize(),
+        beforeSend: function (xhr) {
+            if (posteandoFormulario) {
+                console.log("Ya está subiendo el formulario");
+                xhr.abort();
+                return;
+            }
+            posteandoFormulario = true;
+        },
+        success: function (data) {
+            if (data == "Fallo") {
+                posteandoFormulario = true;
+                $("#hdnPopup").val("FinSession");
+                $("#contenidoPopup").text("Se le ha vencido la sesión, debe volver a iniciar sesión");
+                $("#popup").modal();
+            } else {
+                posteandoFormulario = false;
+            }
+        },
+        error: function () {
+
+        },
+        cache: false
+    });
+}
+
+function postFormGuardar(action) {
+    $.ajax({
+        method: "POST",
+        url: "/HP/Pasos/" + action + "/",
+        data: $("#form").serialize(),
+        beforeSend: function (xhr) {
+            if (posteandoFormulario) {
+                console.log("Ya está subiendo el formulario");
+                xhr.abort();
+                return;
+            }
+            posteandoFormulario = true;
+        },
+        success: function (data) {
+            if (data == "Fallo") {
+                posteandoFormulario = true;
+                $("#hdnPopup").val("FinSession");
+                $("#contenidoPopup").text("Se le ha vencido la sesión, debe volver a iniciar sesión");
+                $("#popup").modal();
+            } else {
+                posteandoFormulario = false;
+                $("#hdnPopup").val() == "GuardadoOK"
+                $("#contenidoPopup").text("Cambios guardados correctamente.");
+                $("#popup").modal();
+            }
+        },
+        error: function () {
+
+        },
+        cache: false
+    });
+}
---
+++ obj/Release/Package/PackageTmp/Scripts/Pagina3.js
@@ -0,0 +1,30 @@
+﻿preguntas =
+[
+    '9. ¿Ha realizado algun tratamiento para modificar su peso anteriormente?',
+    ' ¿Cuanto bajo en los tratamientos anteriores?',
+    ' ¿Tuvo algun efecto negativo en alguno de sus tratamientos anteriores?',
+    ' ¿Porqué considera que sus tratamientos anteriores fracasaron?',
+    '10. ¿Qué barreras cree tener para el tratamiento?',
+    '11. ¿Que cosas cree que lo podrían hacer abandonar este tratamiento?',
+    '12. ¿A que atribuye su peso actual?'
+]
+
+//SOLAMENTE CHECKBOX QUE TENGAN SOLO TEXTO
+checkbox = {
+    pregunta_nueve:['Psiquiatria','Dieta Club','Alco','Psicoterapia',
+        'Actividad Fisica','Homeopatia','Dietas por su cuenta','Medico o Dietista',
+        'Internet(dietas.com)','Internacion','Cirugia','Medicamentos'],
+    pregunta_diez: ['Horarios', 'Desorden', 'Estres', 'Estado de Animo', 'Familia', 'Vida Social', 'Trabajo',
+                     'Motivacion', 'Otras¿Cuales?'
+    ],
+    pregunta_once: ['No modificar peso', 'Problemas personales', 'Vida Social', 'Modificarlo poco', 'Problemas economicos',
+                    'Sentirse presionado', 'Salir una vez y abandonar', 'Llevarse mal con el equipo', 'Aburrirse de la dieta',
+                    'Distancia a la que vive','Poco apoyo familiar','Otras ¿Cuales?'
+    ],
+
+    pregunta_doce: ['Alimentacion', 'Ansiedad', 'Problemas de salud', 'Embarazos', 'Alcohol', 'Estres', 'Metabolismo',
+                    'Herencia', 'Sedentarismo', 'Medicacion', 'Deja de fumar', 'Depresion', 'Pérdidas', 'Menopausia',
+                    'Otros'
+       ]
+}
+
